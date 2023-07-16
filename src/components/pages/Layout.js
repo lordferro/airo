43957a5c0d3f6f45e2e65413df1503c4d3b94cad
Fedor Romanovschi <110813692+lordferro@users.noreply.github.com>
2023-07-16 10:30:30 +0200

    add spiner

--- src/components/pages/Layout.js
+++ src/components/pages/Layout.js
@@ -1,10 +1,24 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
+import { MagnifyingGlass } from "react-loader-spinner";
 
 const Layout = () => {
   return (
     <>
-      <Suspense fallback={<div>loading ...</div>}>
+      <Suspense
+        fallback={
+          <MagnifyingGlass
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="MagnifyingGlass-loading"
+            wrapperStyle={{}}
+            wrapperClass="MagnifyingGlass-wrapper"
+            glassColor="#c0efff"
+            color="#e15b64"
+          />
+        }
+      >
         <Outlet />
       </Suspense>
     </>
